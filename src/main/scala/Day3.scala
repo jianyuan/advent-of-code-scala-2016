5c9ddc549c288942a935672b2b031fc1472fa3c4
Jian Yuan Lee <jianyuan@gmail.com>
2016-12-17 07:22:50 +0800

    Day 3 Part 2

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -5,11 +5,19 @@ object Day3 extends App {
 
   val input = io.Source.fromInputStream(io.Source.getClass.getResourceAsStream("/day3.txt")).getLines().toList
 
-  val triangles = input.map(_.trim.split("""\s+""").map(_.toInt).toList.sorted)
+  val triangles = input.map(_.trim.split("""\s+""").map(_.toInt).toList)
 
   require(triangles.forall(_.size == 3))
 
-  val answer1 = triangles.count(sizes => sizes.init.sum > sizes.last)
-  println(s"How many of the listed triangles are possible? $answer1")
+  def countValidTriangles(triangles: List[List[Int]]): Int = {
+    triangles
+      .map(_.sorted)
+      .count(sizes => sizes.init.sum > sizes.last)
+  }
 
+  val answer1 = countValidTriangles(triangles)
+  println(s"Part 1: How many of the listed triangles are possible? $answer1")
+
+  val answer2 = countValidTriangles(triangles.grouped(3).flatMap(_.transpose).toList)
+  println(s"Part 2: How many of the listed triangles are possible? $answer2")
 }
