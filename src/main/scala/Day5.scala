c34c48382e968ee3c251e62513679c52e4145206
Jian Yuan Lee <jianyuan@gmail.com>
2016-12-17 10:50:21 +0800

    Day 5 Part 2

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -1,5 +1,7 @@
 import java.security.MessageDigest
 
+import scala.annotation.tailrec
+
 /**
   * Created by jianyuanlee on 17/12/2016.
   */
@@ -11,18 +13,32 @@ object Day5 extends App {
 
     def doorIdDigest: MessageDigest = initialDoorIdDigest.clone.asInstanceOf[MessageDigest]
 
-    def hashIndex(i: Int): String = doorIdDigest.digest(i.toString.getBytes).map("%02x".format(_)).mkString
+    def hashForIndex(i: Int): String = doorIdDigest.digest(i.toString.getBytes).map("%02x".format(_)).mkString
   }
 
   val input = "ffykfhsq"
   val doorDigest = DoorDigest(input)
 
-  val answer1 = Stream
+  val combinations: Stream[String] = Stream
     .from(0)
-    .map(doorDigest.hashIndex)
+    .map(doorDigest.hashForIndex)
     .filter(_.substring(0, 5) == "00000")
+
+  val answer1 = combinations
     .take(8)
     .map(_ (5))
     .mkString
   println(s"Given the actual Door ID, what is the password? $answer1")
+
+  @tailrec
+  def solve(combinations: Stream[String], password: List[Option[Char]]): String =
+    if (password.forall(_.isDefined)) password.map(_.get).mkString
+    else combinations match {
+      case combination #:: others if combination(5) <= '7' && password(combination(5) - '0').isEmpty =>
+        solve(others, password.updated(combination(5) - '0', Some(combination(6))))
+      case _ #:: others => solve(others, password)
+    }
+
+  val answer2 = solve(combinations, List.fill(8)(Option.empty[Char]))
+  println(s"Given the actual Door ID and this new method, what is the password? $answer2")
 }
