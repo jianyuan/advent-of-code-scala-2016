6985c25630e55e5e0cf7b84d1e09e6961f52e66a
Jian Yuan Lee <jianyuan@gmail.com>
2016-12-17 11:08:34 +0800

    Day 6 Part 2

--- src/main/scala/Day6.scala
+++ src/main/scala/Day6.scala
@@ -6,5 +6,8 @@ object Day6 extends App {
   val input = io.Source.fromInputStream(io.Source.getClass.getResourceAsStream("/day6.txt")).getLines().toList
 
   val answer1 = input.transpose.map(_.groupBy(identity).maxBy(_._2.size)._1).mkString
-  println(s"What is the error-corrected version of the message being sent? $answer1")
+  println(s"Part 1: $answer1")
+
+  val answer2 = input.transpose.map(_.groupBy(identity).minBy(_._2.size)._1).mkString
+  println(s"Part 2: $answer2")
 }
